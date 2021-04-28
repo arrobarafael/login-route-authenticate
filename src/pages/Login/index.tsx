cd9a2c7650438b18921a19b944d9b3a156ac23c3
Rafael Pelegrini <arrobarafael@hotmail.com>
2021-04-28 14:25:46 -0300

    Ajustes no código

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import styles from './styles.module.scss';
 
 const Login = () => {
+  const history = useHistory();
+
   function handleLogin(event) {
     event.preventDefault();
     console.log('handle login');
+    history.push('home');
   }
 
   return (
     <div className={styles.container}>
-      <form className={styles.login} onSubmit={handleLogin}>
+      <form className={styles.login} onSubmit={handleLogin} action="POST">
         <span>Login</span>
         <input type="text" name="login" />
         <span>Senha</span>
