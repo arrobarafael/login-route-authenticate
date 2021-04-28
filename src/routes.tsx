8f940a76806603b49bba129f5fce227e8a4589a3
Rafael Pelegrini <arrobarafael@hotmail.com>
2021-04-28 13:42:09 -0300

    Adicionada autenticação básica de rota

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { isAuthenticated } from './auth/auth';
 import Home from './pages/Home';
 import Login from './pages/Login';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+      )
+    }
+  />
+);
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Login} />
-        <Route path="/home" component={Home} />
+        <PrivateRoute path="/home" component={Home} />
       </Switch>
     </BrowserRouter>
   );
